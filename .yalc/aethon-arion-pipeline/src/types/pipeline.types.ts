3120db9ac185ab5a6cbfc7d01204014ec82d0f5e
Konstantinos Dimitriou <konstantinos.dimitriou@aethon.sg>
2024-11-08 18:57:25 +0000

    Updated gitignore

--- .yalc/aethon-arion-pipeline/src/types/pipeline.types.ts
+++
@@ -1,3 +0,0 @@
-export type HttpMethod = 'GET' | 'POST' | 'PATCH' | 'DELETE';
-export type StateType = "running" | "pending" | "completed" | "failed";
-export type RandomStreamType = "static" | "random";
